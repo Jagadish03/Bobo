4347ada5ad63e91eaa230cf4ae3e0927316357e4
Jagadish03 <jagadishmunavalli35@gmail.com>
2024-04-24 20:33:30 +0530

    Sidebar

--- boboWeb/src/Pages/Home.jsx
+++ boboWeb/src/Pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam aliquid molestiae dolore, autem itaque numquam illum aut. Modi qui ipsum vel necessitatibus provident ea quas exercitationem dolores sint soluta facilis sit, tempore doloribus deserunt nulla esse magnam natus distinctio! Ad illum possimus deserunt quidem sed, autem voluptatem tenetur iure repudiandae, consequatur unde voluptatibus. Neque aperiam nobis mollitia nostrum sint tenetur voluptatem cum amet reprehenderit itaque veritatis quod exercitationem ea rerum, beatae, accusantium maiores eum fugit doloremque eveniet quas sit expedita! Repellendus aspernatur repudiandae sapiente ratione temporibus quam, est commodi? Blanditiis iusto consequuntur sequi cumque corporis laborum laboriosam! Culpa, rem expedita.</div>
-  )
-}
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam aliquid
+      molestiae dolore, autem itaque numquam illum aut. Modi qui ipsum vel
+      necessitatibus provident ea quas exercitationem dolores sint soluta
+      facilis sit, tempore doloribus deserunt nulla esse magnam natus
+      distinctio! Ad illum possimus deserunt quidem sed, autem voluptatem
+      tenetur iure repudiandae, consequatur unde voluptatibus. Neque aperiam
+      nobis mollitia nostrum sint tenetur voluptatem cum amet reprehenderit
+      itaque veritatis quod exercitationem ea rerum, beatae, accusantium maiores
+      eum fugit doloremque eveniet quas sit expedita! Repellendus aspernatur
+      repudiandae sapiente ratione temporibus quam, est commodi? Blanditiis
+      iusto consequuntur sequi cumque corporis laborum laboriosam! Culpa, rem
+      expedita.
+    </div>
+  );
+};
 
-export default Home
+export default Home;
