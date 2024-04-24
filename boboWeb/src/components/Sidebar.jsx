65af20423a8d9f6d15f0f48e6aa6db351b70541e
Jagadish03 <jagadishmunavalli35@gmail.com>
2024-04-24 21:11:47 +0530

    sidebar

--- boboWeb/src/components/Sidebar.jsx
+++ boboWeb/src/components/Sidebar.jsx
@@ -1,45 +1,67 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Logo from "../assets/BOBO.png";
 import { BsWindowSidebar } from "react-icons/bs";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { BsCarFrontFill } from "react-icons/bs";
 import { GiSteeringWheel } from "react-icons/gi";
+import { LuGripHorizontal } from "react-icons/lu";
+import { RxCross2 } from "react-icons/rx";
 import { GiCash } from "react-icons/gi";
 import { Outlet } from "react-router";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        isOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isOpen]);
+
   return (
     <>
       <div>
         <button
-          data-drawer-target="sidebar-multi-level-sidebar"
-          data-drawer-toggle="sidebar-multi-level-sidebar"
-          aria-controls="sidebar-multi-level-sidebar"
+          onClick={toggleSidebar}
           type="button"
-          className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
         >
           <span className="sr-only">Open sidebar</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              clip-rule="evenodd"
-              fill-rule="evenodd"
-              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-            ></path>
-          </svg>
+          <LuGripHorizontal className="w-6 h-6" />
         </button>
 
+        {/* Sidebar */}
         <aside
-          id="sidebar-multi-level-sidebar"
-          className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+          className={`fixed top-0 left-0 z-40 w-64 h-screen bg-[#101223] transition-transform ${
+            isOpen ? "translate-x-0" : "-translate-x-full"
+          } sm:translate-x-0`}
           aria-label="Sidebar"
         >
-          <div className="h-full px-3 py-4 overflow-y-auto bg-[#101223]">
+          <div className="h-full px-3 py-4 overflow-y-auto">
+            <button
+              onClick={toggleSidebar}
+              className="absolute top-2 right-2 text-gray-400 hover:text-gray-300 focus:outline-none lg:hidden"
+            >
+              <RxCross2 />
+            </button>
             <ul className="space-y-2 font-medium">
 
               <li className="flex items-center justify-center py-16">
@@ -110,24 +132,22 @@ const Sidebar = () => {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
                     />
                   </svg>
                   <span className="flex-1 ms-3 whitespace-nowrap">Logout</span>
                 </a>
-              </li>
-              </div>
+              </li></div>
               
 
-              
-              
             </ul>
           </div>
         </aside>
 
+        {/* Content */}
         <div className="p-4 sm:ml-64">
           <Outlet />
         </div>
